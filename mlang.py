d8286e2cbed6fe3f0de930e6276605196d8182b9
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-10-19 18:54:57 +0300

    operations

--- mlang.py
+++ mlang.py
@@ -1,6 +1,6 @@
 # mlang
 
-# iota counter implementation transpiled from C++
+# basic iota counter
 iotaCounter = 0
 def getIota(shouldReset=False):
     global iotaCounter
@@ -8,3 +8,26 @@ def getIota(shouldReset=False):
         iotaCounter = 0
     iotaCounter += 1
     return iotaCounter - 1
+
+# operations
+PUSH = getIota(True)
+PLUS = getIota()
+MINUS = getIota()
+DUMP = getIota()
+OPCOUNT = getIota()
+
+def push(x):
+    return (PUSH, x)
+
+def plus():
+    return (PLUS, )
+
+def dump():
+    return (DUMP, )
+
+program = [
+    push(31),
+    push(23),
+    plus(),
+    dump()
+]
