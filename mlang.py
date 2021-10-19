f10e23182fe9d1caf3a545824c07379bceebe256
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-10-19 19:00:56 +0300

    basic generator

--- mlang.py
+++ mlang.py
@@ -1,4 +1,5 @@
 # mlang
+import subprocess
 
 # basic iota counter
 iotaCounter = 0
@@ -25,9 +26,26 @@ def plus():
 def dump():
     return (DUMP, )
 
+# generator
+def generate(prg):
+    with open("_tmp.asm", "w") as asm:
+        asm.write("section .text\n")
+        asm.write("global _start\n")
+        asm.write("_start:\n")
+        asm.write("    mov rax, 60\n")
+        asm.write("    mov rdi, 0\n")
+        asm.write("    syscall\n")
+
+    subprocess.call(["nasm", "-felf64", "_tmp.asm"])
+    subprocess.call(["ld", "-o", "program" , "_tmp.o"])
+
+# program
+
 program = [
     push(31),
     push(23),
     plus(),
     dump()
 ]
+
+generate(program)
